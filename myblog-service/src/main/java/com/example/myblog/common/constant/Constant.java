b38d9e81d82d07d38e2c8906c2e04632e9f6851c
duo.tao <taoduo007wow@outlook.com>
2021-05-05 12:11:26 +0800

    补充修改

--- myblog-service/src/main/java/com/example/myblog/common/constant/Constant.java
+++ myblog-service/src/main/java/com/example/myblog/common/constant/Constant.java
@@ -14,29 +14,21 @@ public interface Constant {
 
 
     interface Str {
-        /** 符号：竖线 */
         String VERTICAL_BAR = "|";
-        /** 符号：逗号 */
-        String COMMA        = ",";
-        /** 符号：星号 */
-        String ASTERISK     = "*";
-        /** 符号：减号，连线 */
-        String DASH         = "-";
-        /** 符号：空串 */
-        String EMPTY        = "";
-        /** 符号：斜杠(上坡) */
-        String SLASH        = "/";
-
-        String POUND       = "#";
-        String EMPTY_STRING = "";
-        String AMPERSAND   = "&";
-        String EQUAL       = "=";
-        String QUESTION    = "?";
+        String COMMA = ",";
+        String ASTERISK = "*";
+        String DASH = "-";
+        String EMPTY = "";
+        String SLASH = "/";
+        String POUND = "#";
+        String AMPERSAND = "&";
+        String EQUAL = "=";
+        String QUESTION = "?";
         String EXCLAMATION = "!";
-        String DASH_DASH    = "--";
-        String SEMICOLON   = ";";
-        String UNDERSCORE  = "_";
-        String CRLF        = "\r\n";
+        String DASH_DASH = "--";
+        String SEMICOLON = ";";
+        String UNDERSCORE = "_";
+        String CRLF = "\r\n";
     }
 
     interface Chr {
@@ -45,12 +37,13 @@ public interface Constant {
 
     interface Fields {
         // 公共的常用属性名称
-        String ID          = "id";
-        String IS_DELETED  = "isDeleted";
-        String CREATE_BY   = "createBy";
+        String ID = "id";
+        String IS_DELETED = "isDeleted";
+        String CREATE_BY = "createBy";
         String CREATE_TIME = "createTime";
-        String UPDATE_BY   = "updateBy";
+        String UPDATE_BY = "updateBy";
         String UPDATE_TIME = "updateTime";
+        String REMARK = "remark";
 
     }
 
